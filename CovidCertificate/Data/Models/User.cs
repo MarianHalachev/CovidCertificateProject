b1e2617166e409bd4f98619faddf9011f0348d9d
User <User@DESKTOP-LVSE0NQ>
2022-02-14 14:44:39 +0200

    Add project files.

---
+++ CovidCertificate/Data/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidCertificate.Data.Models
+{
+    public class User:IdentityUser
+    {
+        public User()
+        {
+            this.Schools = new List<School>();
+        }
+
+        public ICollection<School> Schools { get; set; }
+    }
+}
