4d30d4efc6aabd9b3b6ad015bc0ad540f08c92c1
Marian Halachev <mariqn03032@gmail.com>
2022-02-14 14:18:07 +0200

    Add project files.

---
+++ CovidCertificate/Data/Models/School.cs
@@ -0,0 +1,18 @@
+﻿namespace CovidCertificate.Data.Models
+{
+    using CovidCertificate.Data.Models.Enums;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    public class School
+    {
+        public int Id { get; set; }
+        public Student Student { get; set; }
+        public Teacher Teacher { get; set; }
+        public string Name { get; set; }
+        public User User { get; set; }
+        public string Location { get; set; }
+        public TypeOfSchool TypeOfSchool { get; set; }
+    }
+}
